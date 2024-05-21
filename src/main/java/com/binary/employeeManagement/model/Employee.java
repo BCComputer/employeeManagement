86c8cf195ac6513b8c13eb8657e0258f7cf94115
B-Pandey <chitawan2018@gmail.com>
2024-05-21 16:57:42 -0400

    code clean

--- src/main/java/com/binary/employeeManagement/model/Employee.java
+++ src/main/java/com/binary/employeeManagement/model/Employee.java
@@ -3,7 +3,6 @@ package com.binary.employeeManagement.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
